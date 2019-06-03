b07a1f9bbbc5d3defeb3c87d9e17aba4f43ee768
PanchoMrlda <fco.moraledac@gmail.com>
2019-06-03 13:57:22 +0200

    Adding overworld characters and text to PewterMuseumOfScience2F

--- maps/PewterMuseumOfScience2F.asm
+++ maps/PewterMuseumOfScience2F.asm
@@ -3,14 +3,71 @@ PewterMuseumOfScience2F_MapScripts:
 
 	db 0 ; callbacks
 
-PewterMuseumOfScience2FSign1:
-	jumptext PewterMuseumOfScience2FSign1Text
+PewterMuseumYoungsterScript:
+	jumptextfaceplayer PewterMuseumYoungsterText
 
-PewterMuseumOfScience2FSign1Text:
-	text "ROUTE 42"
+PewterMuseumGrampsScript:
+	jumptextfaceplayer PewterMuseumGrampsText
 
-	para "ECRUTEAK CITY -"
-	line "MAHOGANY TOWN"
+PewterMuseumScientistScript:
+	jumptextfaceplayer PewterMuseumScientistText
+
+PewterMuseumLassScript:
+	jumptextfaceplayer PewterMuseumLassText
+
+PewterMuseumFisherScript:
+	jumptextfaceplayer PewterMuseumFisherText
+
+PewterMuseumMoonStoneScript:
+	jumptextfaceplayer PewterMuseumMoonStoneText
+
+PewterMuseumSpaceShuttleScript:
+	jumptextfaceplayer PewterMuseumSpaceShuttleText
+
+PewterMuseumYoungsterText:
+	text "MOON STONE?"
+
+	para "What's so special"
+	line "about it?"
+	done
+
+PewterMuseumGrampsText:
+	text "July 20, 1969!"
+
+	para "The 1st lunar"
+	line "landing!"
+
+	para "I bought a color"
+	line "TV to watch it!"
+	done
+
+PewterMuseumScientistText:
+	text "We have a space"
+	line "exhibit now."
+	done
+
+PewterMuseumLassText:
+	text "I want a PIKACHU!"
+	line "It's so cute!"
+
+	para "I asked my Daddy"
+	line "to catch me one!"
+	done
+
+PewterMuseumFisherText:
+	text "Yeah, a PIKACHU"
+	line "soon, I promise!"
+	done
+
+PewterMuseumSpaceShuttleText:
+	text "SPACE SHUTTLE"
+	line "COLUMBIA"
+	done
+
+PewterMuseumMoonStoneText:
+	text "Meteorite that"
+	line "fell on MT.MOON."
+	cont "(MOON STONE?)"
 	done
 
 PewterMuseumOfScience2F_MapEvents:
@@ -22,6 +79,12 @@ PewterMuseumOfScience2F_MapEvents:
 	db 0 ; coord events
 
 	db 1 ; bg events
-	bg_event  2,  5, BGEVENT_READ, PewterMuseumOfScience2FSign1
+	bg_event  2,  5, BGEVENT_READ, PewterMuseumMoonStone
+	bg_event 11,  2, BGEVENT_READ, PewterMuseumSpaceShuttle
 
-	db 0 ; object events
+	db 5 ; object events
+	object_event  1,  4, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 2, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PewterMuseumYoungsterScript, -1
+	object_event  0,  5, SPRITE_GRAMPS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PewterMuseumGrampsScript, -1
+	object_event  7,  5, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PewterMuseumScientistScript, -1
+	object_event 11,  5, SPRITE_LASS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PewterMuseumLassScript, -1
+	object_event 12,  5, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, PewterMuseumFisherScript, -1
