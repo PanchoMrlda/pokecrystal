a767d7e4101250345a04c79b6f11a250036fea2c
PanchoMrlda <fco.moraledac@gmail.com>
2019-06-11 11:54:02 +0200

    Replacing Poliwag with Poliwrath sprite in CeladonCity

--- data/maps/outdoor_sprites.asm
+++ data/maps/outdoor_sprites.asm
@@ -97,7 +97,7 @@ CeladonGroupSprites:
 	db SPRITE_LASS
 	db SPRITE_BIKER
 	; 6 of max 9 walking sprites
-	db SPRITE_POLIWAG
+	db SPRITE_POLIWRATH
 	db SPRITE_POKE_BALL
 	db SPRITE_FRUIT_TREE
 	db 0 ; end
