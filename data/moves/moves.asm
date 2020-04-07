4e0adf721db1a83129abc4496141a549b6b7cf8d
PanchoMrlda <fco.moraledac@gmail.com>
2020-04-07 09:05:38 +0200

    Updating vine whip attack power and pp

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -33,7 +33,7 @@ Moves: ; 41afb
 	move FLY,          EFFECT_FLY,                70, FLYING,   PHYSICAL,  95, 15,   0
 	move BIND,         EFFECT_TRAP_TARGET,        30, NORMAL,   PHYSICAL,  75, 20,   0
 	move SLAM,         EFFECT_NORMAL_HIT,         80, NORMAL,   PHYSICAL,  75, 20,   0
-	move VINE_WHIP,    EFFECT_NORMAL_HIT,         35, GRASS,    PHYSICAL, 100, 10,   0
+	move VINE_WHIP,    EFFECT_NORMAL_HIT,         45, GRASS,    PHYSICAL, 100, 25,   0
 	move STOMP,        EFFECT_STOMP,              65, NORMAL,   PHYSICAL, 100, 20,  30
 	move DOUBLE_KICK,  EFFECT_DOUBLE_HIT,         30, FIGHTING, PHYSICAL, 100, 30,   0
 	move MEGA_KICK,    EFFECT_NORMAL_HIT,        120, NORMAL,   PHYSICAL,  75,  5,   0
